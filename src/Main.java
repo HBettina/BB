ac41f8124a1707740888bfa988b382b08c708175
Betti <hbetyka@gmail.com>
2023-01-02 13:34:15 +0100

    min value

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,17 @@ public class Main {
         for (int i = 0; i < randomNumbers.length; i++) {
             randomNumbers[i] = (int) ((Math.random() * 100) + 1);
         }
+        System.out.println(getMinimum(randomNumbers));
         System.out.println("hello world!");
         System.out.println("new git version");
+    }
+    public static int getMinimum(int[] randomnumbers){
+        int minNumber = randomnumbers[0];
+        for (Integer number: randomnumbers) {
+            if(number < minNumber) {
+                minNumber = number;
+            }
+        }
+        return minNumber;
     }
 }
